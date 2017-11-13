8aefaf0c176206eafb3880892a8d3309c864f641
Christian Panadero <panavtec@gmail.com>
2017-11-13 21:07:44 +0000

    Add packagegroup return failures/success

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,40 @@
 module Main where
 
 import           Control.Monad.IO.Class
+import           Data.Function
+import           Data.List
+import qualified Data.Map               as Map
 import           Data.Text              (pack)
 import           SystemUpdater.Data
 import           Turtle
 
+
+data PackageGroup = PackageGroup {
+                      id    :: String,
+                      state :: State}
+data State = Success | Failure [YaourtPackage]
+
 main :: IO ()
 main = do
-  yaourt [YaourtPackage "shunit2", YaourtPackage "shellcheck-static"]
   return ()
 
-yaourt :: [YaourtPackage] -> IO [ExitCode]
-yaourt = sequence . fmap installSingle
+bash :: IO PackageGroup
+bash = toPackageGroup <$> failurePackages
+  where
+    toPackageGroup []  = PackageGroup id Success
+    toPackageGroup pkg = PackageGroup id (Failure pkg)
+    failurePackages = fmap snd <$> Map.toList <$> snd <$> Map.partitionWithKey (\k _ -> k == ExitSuccess) <$> Map.fromList <$> processedPackages
+    processedPackages :: IO [(ExitCode, YaourtPackage)]
+    processedPackages = flip zip packages <$> yin packages
+    id = "Bash"
+    packages = [YaourtPackage "shunit2", YaourtPackage "shellcheck-static"]
+
+yin :: [YaourtPackage] -> IO [ExitCode]
+yin = sequence . fmap installSingle
   where installSingle (YaourtPackage x) = shell (pack $ "yaourt -S --noconfirm " ++ x) empty
+
+--  installGo() {
+--  yaourt --noconfirm -S go
+--  mkdir -p "$HOME/go/{bin,src}"
+--  go get -u github.com/golang/lint/golint
+--}

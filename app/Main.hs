8a4ae1be9da0c9e3b5503316d86e9f57f25ccb50
Christian Panadero <panavtec@gmail.com>
2017-11-14 13:50:31 +0000

    Migrate zip to yin and inline toPackageGroup inner functions

--- app/Main.hs
+++ app/Main.hs
@@ -7,33 +7,33 @@ import           Data.Function
 import           Data.List
 import qualified Data.Map               as Map
 import           Data.Text              (pack)
+import           Data.Tuple
 import           SystemUpdater.Data
 import           Turtle
 
-
-data PackageGroup = PackageGroup {
-                      id    :: String,
-                      state :: State}
+data PackageGroup = PackageGroup {id    :: String, state :: State}
 data State = Success | Failure [YaourtPackage]
 
 main :: IO ()
 main = do
   return ()
 
+-- refactorizar este codigo para extraer la logica relacionada
+-- con el mapping de exitcodes a packagroup y razonar si esta logica no deberia
+-- de estar en `yin`
 bash :: IO PackageGroup
-bash = toPackageGroup <$> failurePackages
+bash = toPackageGroup <$> fmap snd <$> Map.toList <$> snd <$> Map.partitionWithKey (\k _ -> k == ExitSuccess) <$> Map.fromList <$> fmap swap <$> yin packages
   where
     toPackageGroup []  = PackageGroup id Success
     toPackageGroup pkg = PackageGroup id (Failure pkg)
-    failurePackages = fmap snd <$> Map.toList <$> snd <$> Map.partitionWithKey (\k _ -> k == ExitSuccess) <$> Map.fromList <$> processedPackages
-    processedPackages :: IO [(ExitCode, YaourtPackage)]
-    processedPackages = flip zip packages <$> yin packages
     id = "Bash"
     packages = [YaourtPackage "shunit2", YaourtPackage "shellcheck-static"]
 
-yin :: [YaourtPackage] -> IO [ExitCode]
-yin = sequence . fmap installSingle
-  where installSingle (YaourtPackage x) = shell (pack $ "yaourt -S --noconfirm " ++ x) empty
+yin :: [YaourtPackage] -> IO [(YaourtPackage, ExitCode)]
+yin pkgs = zip pkgs <$> installGroup pkgs
+  where
+    installGroup pkgs = sequence $ installSingle <$> pkgs
+    installSingle (YaourtPackage x) = shell (pack $ "yaourt -S --noconfirm " ++ x) empty
 
 --  installGo() {
 --  yaourt --noconfirm -S go

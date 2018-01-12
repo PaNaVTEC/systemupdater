82fdfee7e1a5df94fe1b9ed4f07e7bb87dd4e809
Christian Panadero <panavtec@gmail.com>
2018-01-12 19:13:17 +0000

    Add intallGit function + lnsfn

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,20 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Function
 import           Data.List
-import qualified Data.Map               as Map
-import           Data.Text              (pack, unpack)
+import qualified Data.Map                  as Map
+import           Data.Text                 (Text, pack, unpack)
 import           Data.Tuple
+import           Filesystem.Path.CurrentOS (encodeString)
 import           SystemUpdater.Data
 import           Turtle
+
 
 data PackageGroupExitState = Success String | Failure String [YaourtPackage]
 type PackageExitCode = (YaourtPackage, ExitCode)
 
 main :: IO ()
-main = return ()
+main = do
+  return ()
 
 bash :: IO PackageGroupExitState
 bash = toState "Bash" . failedPackages <$> yin packages
@@ -50,3 +53,22 @@ yin pkgs = zip pkgs <$> installGroup pkgs
   where
     installGroup pkgs = sequence $ installSingle <$> pkgs
     installSingle (YaourtPackage x) = shell (pack $ "yaourt -S --noconfirm " ++ x) empty
+
+installGit :: IO ()
+installGit = do
+  failPackages <- toState "Git" . failedPackages <$> yin packages
+  lnsfn (fromText "./config/git/gitconfig") (fromText "~/.gitconfig")
+  --TODO: config gibo
+  return ()
+  where
+    packages = [YaourtPackage "diff-so-fancy", YaourtPackage "gibo"]
+
+lnsfn :: Turtle.FilePath -> Turtle.FilePath -> IO ExitCode
+lnsfn s d = shell (pack $ "ln -sfn " ++ (encodeString s) ++  " " ++ (encodeString d)) empty
+
+--installGit() {
+--  # Global git ignores
+--  gibo --upgrade
+--  gibo Emacs Vim JetBrains Ensime Tags Vagrant Windows macOS Linux Archives >> ~/.gitignore.global
+--  echo ".tern-project" >> ~/.gitignore.global
+--}
